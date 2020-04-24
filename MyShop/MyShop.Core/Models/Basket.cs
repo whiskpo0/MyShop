d56c9f152bfe27c9648770a1b7b5999a3741c892
Adam M <whiskypo082@gmail.com>
2020-04-24 15:51:51 -0500

    Added Basket Model

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        public virtual ICollection<BasketItem> BasketItem { get; set; }
+
+        public Basket()
+        {
+            this.BasketItem = new List<BasketItem>(); 
+        }
+    }
+}
